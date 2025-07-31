fa13fe326670a59ecc1774906494806c9c43b520
codewithdhanus <167870538+codewithdhanus@users.noreply.github.com>
2025-07-31 23:13:59 +0530

    Initial commit

---
+++ components/upgrade-modal.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+
+// If you want to keep the structure but disable display
+export function UpgradeModal() {
+  return null; // disables the upgrade modal completely
+}
